c29d290b110faa67809f6a6be8d00d172177dfed
Tarun yadav <tarunyadav9761@gmail.com>
2021-02-12 09:35:03 +0000

    folder stucture fix

---
+++ src/pages/Auth/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+    return (
+        <div>
+            This is A SignIn Page
+        </div>
+    )
+}
+
+export default SignIn
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div>
+            This is a Dashboard
+        </div>
+    )
+}
+
+export default Dashboard
