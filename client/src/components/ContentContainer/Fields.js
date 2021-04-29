498fc4fc300144133fdf92172985c91909bb87e1
Naman-Srivastava26 <naman.17bcs1142@abes.ac.in>
2021-04-29 18:16:06 +0530

    oauth, minor fixes

---
+++ client/src/components/ContentContainer/Fields.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { blue } from "@material-ui/core/colors";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import FieldImage from "../../static/images/farm.jpg";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+    padding: "20px 20px",
+    background: "#0cedc8",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: blue[500],
+  },
+  typo: {
+    padding: "0px 2px",
+  },
+}));
+
+const FieldsCardlay = ({ data }) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            F
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={data.fieldname}
+        subheader={data.epoch}
+      />
+      <CardMedia className={classes.media} image={FieldImage} title="Fields" />
+      <CardContent>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="ul"
+          className={classes.typo}
+        >
+          <li> UserId : {data.uid}</li>
+          <li> Geolocation : {data.geo}</li>
+          <li> Time : {data.time}</li>
+          <li> Network_Strength : {data.net}</li>
+        </Typography>
+      </CardContent>
+      {/* <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent></CardContent>
+      </Collapse> */}
+    </Card>
+  );
+};
+export default FieldsCardlay;
